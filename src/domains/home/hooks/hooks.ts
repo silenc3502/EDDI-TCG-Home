ceb2d07a3e8355d5705ba50fa490eb255b6edf3b
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2026-02-14 01:51:22 +0900

    feat: [이재림] 메인 화면의 IntroSection 기능 및 행동 로직 구현 [ETH-7]
    - IntroSection 데이터 모델 및 상태 구조 정의
    - CTA 클릭 시 사용자 행동에 대한 기본 처리 로직 구현

--- src/domains/home/hooks/hooks.ts
+++ src/domains/home/hooks/hooks.ts
@@ -20,8 +20,25 @@ const DEFAULT_MODEL: HomeModel = {
     },
 
     intro: {
-        headline: '세계관 소개',
-        description: '…',
+        headline: [
+            '부서진 순리,',
+            '깨어난 전쟁'
+        ],
+        description:[
+            [
+                '끝없는 겨울이 내려오고 죽음이 쌓여가자, 인간은 생존을 위해 시체를 부활시켰다.'
+            ],
+            [
+                '되살아난 것은 생명이 아닌 죽음을 거부한 언데드였다.',
+                '자연의 순리는 왜곡되었고 회복은 멈췄다.',
+                '그 균열을 감지한 자연의 일부, 트랜트는 깨어났다.'
+            ],
+            [
+                '완전한 부활을 원하는 언데드, 질서를 지키려는 트랜트, 살아남고자 하는 인간',
+                '그들의 선택은 결국 전쟁으로 이어졌다.'
+            ]
+        ],
+        primaryAction: '세계관 더 알아보기'
     },
 };
 
@@ -48,9 +65,14 @@ export function useHome() {
         alert('현재 준비중인 페이지입니다.');
     }
 
+    function onIntroPrimaryAction() {
+        alert('현재 준비중인 페이지입니다.');
+    }
+
     return {
         state,
         initHome,
         onHeroPrimaryAction,
+        onIntroPrimaryAction,
     };
 }

16f39c519290dd723e90adf4005b9e1ed627f212
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2026-02-21 01:35:38 +0900

    refactor: [이재림] home의 hooks에서 모델 정의 분리 [ETH-5, 7]

--- src/domains/home/model/model.ts
+++ src/domains/home/model/model.ts
@@ -4,12 +4,12 @@ export interface HomeModel {
         description: string[];
 
         background: {
-            tone: 'dark-fantasy'; // 배경효과 변경 가능성
+            tone: 'dark-fantasy'; // 배경효과 변경 가능성(enum)
         };
 
         primaryAction: {
             label: string;
-            type: 'ENTER_GAME'; // 로그인 여부에 따라서 타입이 달라짐
+            type: 'ENTER_GAME'; // 로그인 여부에 따라서 타입이 달라짐(union type)
         };
     };
 
